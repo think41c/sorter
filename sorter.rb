a74902d971f49dc922a4df0d149607c5a5cb796b
Tony DiNitto <think41c@gmail.com>
2015-09-23 00:28:32 -0400

    Removed extra puts status statements.

--- sorter.rb
+++ sorter.rb
@@ -7,17 +7,12 @@ class Sorter
         done_sort = true 
     	  sequence.each_with_index do |x,i|
           previous = sequence[i]
-          puts "previous is #{previous}"
           current  = sequence[i+1]
-          puts "current is #{current}"
           if current != nil && previous > current
-            puts "#{current} and #{previous} were > than #{current}"
             sequence[i+1] = previous
             sequence[i]   = current
             done_sort     = false
           end
-          puts "\n"
-          
         end
       end
     result = sequence.join(" ")
@@ -26,4 +21,4 @@ class Sorter
 end
 
 a = Sorter.new
-a.sort([6,3])
+a.sort([6,3, 1])
